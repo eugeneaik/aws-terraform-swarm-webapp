6d35d5c4ede44e10012b6dfa82fea2a577060a78
Eugene Aikashev <eugene.aikashev@gmail.com>
2018-07-17 16:49:30 +0300

    updates

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template
 import docker
 
-client = docker.DockerClient(base_url='unix://var/run/docker.sock', version='1.24') 
+client = docker.DockerClient(base_url='unix://var/run/docker.sock', version='auto') 
 container_dict = {}
 service_dict = {}
 
@@ -14,6 +14,7 @@ def get_containers():
 def get_services():
     for i_service in client.services.list():
         service_dict[i_service.id] = i_service.attrs['Spec']['TaskTemplate']['ContainerSpec']['Image']
+
 def get_logs(id):
     container = client.containers.get(id)
     return container.logs()
@@ -23,7 +24,6 @@ def mainpage():
     version = (client.version()).get('Version')
     apiver = (client.version()).get('ApiVersion')
     get_containers()
-    get_services()
     return render_template('index.html',version=version, api=apiver, dcontainer=container_dict, dservice=service_dict)
 
 @app.route('/container/<string:id>')
